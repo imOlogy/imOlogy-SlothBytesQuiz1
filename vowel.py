243efcb5eac42df4a41571d5699cae38fac8746d
imOlogy <ahmadmology1@gmail.com>
2024-06-11 19:22:27 +0300

    Forgot to write it as a function

--- vowel.py
+++ vowel.py
@@ -1,11 +1,13 @@
-word= input("Enter your word: ")
-word= word.lower()
-r= len(word)
-vowels= 0
+def count_vowels():
+    word= input("Enter your word: ")
+    word= word.lower()
+    r= len(word)
+    vowels= 0
+    for i in range(r):
+        if word[i]=="a" or "e" or "i" or "o" or "u":
+            vowels+=1
 
-for i in range(r):
-    if word[i]=="a" or word[i]=="e" or word[i]=="i" or word[i]=="o" or word[i]=="u":
-        vowels+=1
+    if vowels== 1: print(str(vowels) + " vowel")
+    else: print(str(vowels)+ " vowels")
 
-if vowels== 1: print( str(vowels) + " vowel")
-else: print(str(vowels)+ " vowels")
+count_vowels()
